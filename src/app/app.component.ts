c765f3dc70fde09838b95edb063745dfc06412b9
Alex <alexmdalton@sbcglobal.net>
2022-08-14 14:42:09 -0500

    Completed all bonus missions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { type } from 'os';
 import { Satellite } from './satellite';
 
 @Component({
@@ -42,7 +43,13 @@ export class AppComponent {
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
 			let name = this.sourceList[i].name.toLowerCase();
+			let type = this.sourceList[i].type.toLowerCase();
+			let orbitType = this.sourceList[i].orbitType.toLowerCase();
 			if (name.indexOf(searchTerm) >= 0) {
+				matchingSatellites.push(this.sourceList[i]);
+			} else if(type.indexOf(searchTerm) >= 0) {
+				matchingSatellites.push(this.sourceList[i]);
+			} else if(orbitType.indexOf(searchTerm) >= 0){
 				matchingSatellites.push(this.sourceList[i]);
 			}
 		}
